b8043fe833f275a291ea3537eaa0b068facf41cf
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2024-03-04 09:18:45 +0530

    Completed Emojipedia

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Entry from "../src/components/Entry";
+import emojipedia from "../src/components/emojipedia";
+
+//1. Extract the repeated parts of the App into a Entry component.
+//2. Use props to make the Entry component render different data.
+//3a. Import the emojipedia constant.
+//3b. Map through the emojipedia array and render Entry components.
+
+//Emojipedia has 3 entries, so createEntry gets called 3 times.
+//Each time, it passes 1 item from the emojipedia array as a var called emojiTerm.
+
+//var emojiTerm = {
+//   id: 1,
+//   emoji: "💪",
+//   name: "Tense Biceps",
+//   meaning:
+//     "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+// }
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
--- src/components/emojipedia.js
+++ src/components/emojipedia.js
@@ -1,33 +1,46 @@
 const emojipedia = [
     {
-      id: 1,
-      emoji: "💪",
-      name: "Tense Biceps",
-      meaning:
-        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+        id: 1,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
     },
     {
-      id: 2,
-      emoji: "🙏",
-      name: "Person With Folded Hands",
-      meaning:
-        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+        id: 2,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning:
+            "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
     },
     {
-      id: 3,
-      emoji: "🤣",
-      name: "Rolling On The Floor, Laughing",
-      meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        id: 3,
+        emoji: "🤣",
+        name: "Rolling On The Floor, Laughing",
+        meaning:
+            "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
     },
     {
-      id: 4,
-      emoji: "🤓",
-      name: "Nerd Face",
-      meaning:
-        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+        id: 4,
+        emoji: "🤓",
+        name: "Nerd Face",
+        meaning:
+            "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+        id: 5,
+        emoji: "🍗",
+        name: "Chicken Legs",
+        meaning:
+            "I’m hungry! We’re going to KFC! Crispy on the outside, juicy on the inside: The bottom part of a poultry is grilled, baked or fried. Popular fast food dish."
+    },
+    {
+        id: 6,
+        emoji: "🍟",
+        name: "French Fries",
+        meaning:
+            "We're going to the chippy! Ingredient of national dishes, for instance, with mussels in France or fish and chips in the UK. Fast food, as a side dish and globally a popular frozen product."
     }
-  ];
-  
-  export default emojipedia;
-  
+];
+
+export default emojipedia;
